ae3cdf6aaaa91e1c97abfe05384d9750f6059397
Jakob Germann <jakobgermann@comcast.net>
2021-04-07 01:07:02 -0400

    Command Updates 1
    Added the following commands:
    - Getting the current working directory
    - Change your current directory to another one, without actually moving your location in the system.
    - Making a new file in the current directory
    - Checking if a file or directory exists(Needs work)

--- mysh.c
+++ mysh.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <dirent.h>
 
 #define COMMANDSIZE 500
 #define COMMANDHISTORY 100
@@ -20,6 +21,13 @@ typedef struct History{
 
 void commandInterfaceHandler();
 void checkIfFileExists(char *fileName);
+int checkInputType(char *tokenizedInput);
+char *getCurrentDirectory();
+void changeCurrentDirectory(char *tokenizedInput);
+void makeNewFile(char *tokenizedInput);
+
+char currentDir[256];
+
 
 int main(int argc, char const *argv[])
 {
@@ -36,6 +44,8 @@ void commandInterfaceHandler(){
     char *command = (char*)malloc(COMMANDSIZE * sizeof(char));
     char *delimiter = "\t\n ";
     history *myHistory = (history*)malloc(COMMANDHISTORY * sizeof(history));
+
+    getCurrentDirectory();
 
     while(1){
 
@@ -59,16 +69,87 @@ void commandInterfaceHandler(){
             tokenizedInput = strtok(NULL, delimiter); //Get the file name
 
             //Check if it's a file or directory
-            //checkInputType(tokenizedInput);
+            int type = checkInputType(tokenizedInput);
 
-            checkIfFileExists(tokenizedInput);
+            if(type == 1){
+
+                //Directory type
+                printf("Abode is.\n");
+
+            } else {
+
+                checkIfFileExists(tokenizedInput);
+
+            }
+        } else if(strcmp(tokenizedInput, "whereami") == 0){
 
             
+            printf("Current Directory: %s\n", currentDir); //Display current working directory
+
+        } else if(strcmp(tokenizedInput, "movetodir") == 0){
+
+            tokenizedInput = strtok(NULL, delimiter);
+
+            changeCurrentDirectory(tokenizedInput);
+
+        } else if(strcmp(tokenizedInput, "maik") == 0){
+
+            tokenizedInput = strtok(NULL, delimiter); //Get the file name
+
+            makeNewFile(tokenizedInput);
         }
         
     }
 }
 
+
+void makeNewFile(char *tokenizedInput){
+
+    if(access(tokenizedInput, F_OK) == 0){
+
+        printf("File already exists.\n");
+
+    } else {
+
+        FILE *fp = fopen(tokenizedInput, "w");
+
+        fprintf(fp, "Draft");
+        fclose(fp);
+    }
+}
+
+
+void changeCurrentDirectory(char *tokenizedInput){
+
+    DIR *directory = opendir(tokenizedInput);
+
+    if(directory){
+
+        strcpy(currentDir, tokenizedInput);
+
+        closedir(directory);
+
+    } else {
+
+        printf("Directory does not exist.\n");
+
+    }
+}
+
+
+int checkInputType(char *tokenizedInput){
+
+    struct stat path_stat;
+
+    if(S_ISDIR(path_stat.st_mode) && !stat(tokenizedInput, &path_stat)){
+
+        return 1; //It's a directory
+
+    } else {
+
+        return 2; //It's a file
+    }
+}
 
 void checkIfFileExists(char *fileName){
 
@@ -83,3 +164,12 @@ void checkIfFileExists(char *fileName){
     }
 }
 
+
+char *getCurrentDirectory(){
+
+    if(getcwd(currentDir, sizeof(currentDir)) == NULL){
+
+        perror("getcwd error");
+
+    }
+}
